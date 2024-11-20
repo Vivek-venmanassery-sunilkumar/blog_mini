3b28a9654dbf4f4ef88cd34e2c6da472555bf0fc
kid <venmanassery.vivek@gmail.com>
2024-11-20 11:03:27 +0530

    task completed but project yet to be completed

--- blog_main/admin_auth/urls.py
+++ blog_main/admin_auth/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'admin_auth'
 
 urlpatterns = [
-    path('', views.auth_admin, name = 'admin')
+    path('', views.auth_admin, name = 'admin'),
+    path('edit/<int:user_id>/', views.edit_user, name='edit_user'),
 ]
+
--- blog_main/admin_auth/views.py
+++ blog_main/admin_auth/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.models import User
 from django.views.decorators.cache import cache_control
+from .forms import UserEditForm
 
 # Create your views here.
 @cache_control(no_cache=True,must_revalidate=True, no_store=True)
 def auth_admin(request):
     if request.method=='POST':
         action = request.POST.get('action')
+        pk = request.POST.get('user_id')
+        user = get_object_or_404(User, pk=pk)
         if action == 'delete':
-            pk = request.POST.get('user_id')
-            user = User.objects.get(pk=pk)
-            if user.is_superuser:
-                print('delete cannot be performed')
-            else:
+            if not user.is_superuser:
                 user.delete()
-        elif action == 'edit':
-            pk = request.POST.get('user_id')
-            user = User.objects.get(pk=pk)
-            pass
-        return redirect('admin_auth:admin')
+            return redirect('admin_auth:admin')
+        # elif action == 'edit':
+        #     form = UserEditForm(request.POST, instance=user)
+        #     if form.is_valid():
+        #         form.save()
+        #         return redirect('auth_admin:admin')
+        #     else:
+        #         return render(request, 'edit_user.html', {'form':form, 'user_id':pk})
+        
     else:
         if request.user.is_authenticated and request.user.is_superuser:
             user = request.user.username
@@ -29,3 +32,16 @@ def auth_admin(request):
         else:
             return redirect('blog_homepage:homepage')
     
+def edit_user(request, user_id):
+    user = get_object_or_404(User, pk = user_id)
+    if request.method == 'POST':
+        form = UserEditForm(request.POST, instance = user)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_auth:admin')
+        else:
+            form = UserEditForm(instance = User)
+        return render(request, 'edit_user.html', {'form':form, 'user_id':user_id})
+    else:
+        form = UserEditForm(instance = user)
+        return render(request, 'edit_user.html', {'form':form, 'user_id':user_id})
